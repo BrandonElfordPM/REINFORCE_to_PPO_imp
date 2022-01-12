b430ab98e38b70a02bb288f8b25935b6bb3cddbc
BrandonElfordPM <brandonelford@potentialmotors.com>
2022-01-12 18:30:37 -0400

    FEAT: cleaning REINFORCE and retraining to confirm it is working;

--- REINFORCE.py
+++ REINFORCE.py
@@ -6,13 +6,18 @@ import gym
 
 import torch 
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim import Adam
+import torch.nn.functional as F
 from torch.distributions import Categorical
 
+###
 
 class Policy(nn.Module):
-    def __init__(self, input_dim, hidden_layers, output_dim):
+    def __init__(self, 
+                 input_dim: int=1, 
+                 hidden_layers: list= [8], 
+                 output_dim: int=1):
         super(Policy, self).__init__()
         # construct neural net
         self.layers = []
@@ -69,65 +74,122 @@ class Policy(nn.Module):
 
 ###
 
-def REINFORCE(env, hidden_layers, learning_rate, discount_rate):
-    """
-        defines the environment, initializes the policy, simulates trajectories 
-        of each episode, computes the loss and updates the policy.
-    """
-    epi_rewards = []
-    # if the env space is discrete, we need to use 'n' instead because 'shape returns empty
-    try:
-        input_dim = env.observation_space.shape[0]
-    except:
-        input_dim = env.observation_space.n
-    try:
-        output_dim = env.action_space.shape[0]
-    except:
-        output_dim = env.action_space.n
-    # construct policy
-    pi  = Policy(input_dim, hidden_layers, output_dim)
-    # construct optimizer
-    opt = optim.Adam(pi.parameters(), lr=learning_rate)
-    # for each episode...
-    for epi in range(EPISODE_NUM):
-        state = env.reset()
+class REINFORCE:
+    def __init__(self, 
+                 env: gym.Env, 
+                 policy: nn.Module = None, 
+                 policy_kwargs: dict = {}, 
+                 optimizer: torch.optim = None, 
+                 hyper_params: dict = {},
+                 model_name: str = None,
+                 ):
+        self.env = env
+        # if the env space is discrete, we need to use 'n' instead because 'shape returns empty
+        try:
+            self.observation_space = env.observation_space.shape[0]
+        except:
+            self.observation_space = env.observation_space.n
+        try:
+            self.action_space = env.action_space.shape[0]
+        except:
+            self.action_space = env.action_space.n
+        # hyperparams
+        self._lr            = hyper_params['lr']            if 'lr'            in hyper_params else 3e-3
+        self._gamma         = hyper_params['gamma']         if 'gamma'         in hyper_params else 0.99
+        self._hidden_layers = hyper_params['hidden_layers'] if 'hidden_layers' in hyper_params else [32]
+        # construct policy
+        if not policy:
+            self.policy = Policy(self.observation_space, 
+                                 self._hidden_layers, 
+                                 self.action_space)
+        else:
+            self.policy = policy
+        # construct optimizer
+        if not optimizer:
+            self.optimizer = optim.Adam(self.policy.parameters(), 
+                                        lr=self._lr)
+        else:
+            self.optimizer = optimizer
+        # load weights
+        if model_name:
+            self.load(file_name=model_name)
+
+        self.episode_rews = []
+
+    def do_epoch(self, 
+                 max_timestep: int = 5e2):
+        state = self.env.reset()
         # sample a trajectory
-        for t in range(MAX_TIMESTEP):
-            action = pi.act(state)
-            state, reward, done, _ = env.step(action)
-            pi.rewards.append(reward)
+        for t in range(max_timestep):
+            action = self.policy.act(state)
+            state, reward, done, _ = self.env.step(action) # [action] for Pendulum, action otherwise
+            self.policy.rewards.append(reward)
             # env.render()
             if done:
                 break
-        # compute discounted rewards
-        T = len(pi.rewards)
+        # convert step rewards to cumulative rewards
+        rewards = self.compute_cumulative_rewards()
+        # gradient loss
+        log_probs = torch.stack(self.policy.log_prob)
+        loss = torch.sum( -log_probs * rewards )
+        # update optimizer
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        # recording episode rewards
+        self.episode_rews.append(sum(self.policy.rewards))
+        # print out episode stats
+        avg_epi_rew = np.mean(self.episode_rews[-10:])
+        return avg_epi_rew, loss
+
+    def do_n_epochs(self, 
+                    num_episodes: int = 1e3,
+                    max_timestep: int = 5e2,
+                    rew_threshold: int = 5e2 // (9/10),
+                    save_model: bool = False
+                    ):
+        # for each episode...
+        for epi in range(num_episodes):
+            avg_epi_rew, loss = self.do_epoch(max_timestep)
+            if epi % 100 == 0:
+                print("\nEpisode: {}\nLoss:    {}\nAvg Epi Rew: {}".format(epi, loss, avg_epi_rew))
+            # checking if solved
+            if avg_epi_rew > rew_threshold:
+                print("SOLVED")
+                break
+            # reseting policy for next episode
+            self.policy.reset()
+        if save_model:
+            print('\nSaving...\n')
+            self.save(model_name)
+
+    def compute_cumulative_rewards(self):
+        # compute cumulative rewards
+        T = len(self.policy.rewards)
         rewards = np.empty(T, dtype=np.float32)
         next_rew = 0.0
         for t in range(T):
-            next_rew = pi.rewards[t] + discount_rate * next_rew
+            next_rew = self.policy.rewards[t] + self._gamma * next_rew
             rewards[t] = next_rew
         rewards = torch.tensor(rewards)
-        # gradient loss
-        log_probs = torch.stack(pi.log_prob)
-        loss = torch.sum( -log_probs * rewards )
-        # update optimizer
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-        # recording episode rewards
-        epi_rewards.append(sum(pi.rewards))
-        # print out episode stats
-        avg_epi_rew = np.mean(epi_rewards[-10:])
-        if epi % 100 == 0:
-            print("\nEpisode: {}\nLoss:    {}\nAvg Epi Rew: {}".format(epi, loss, avg_epi_rew))
-        # checking if solved
-        if avg_epi_rew > REW_THRWSHOLD:
-            print("SOLVED")
-            break
-        # reseting policy for next episode
-        pi.reset()
-    return pi
-    
+        return rewards
+
+    def save(self,
+             env_name: str,
+             model_name: str='model_ckpt',
+             ):
+        file_name = self.env.unwrapped.spec.id+'-'+model_name
+        torch.save({'model_state_dict': self.policy.model.state_dict(), 
+                    'optim_state_dict': self.optimizer.state_dict()},
+                   file_name)
+
+    def load(self,
+             file_name: str = 'module_ckpt',
+             ):
+        state_dict = torch.load(file_name)
+        self.policy.model.load_state_dict(state_dict['model_state_dict'])
+        self.optim.load_state_dict(state_dict['optim_state_dict'])
+
 ###
 
 def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
@@ -140,38 +202,47 @@ def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
     anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
     anim.save(path + filename, writer='pillow', fps=60)
 
-def valid(pi, env_name):
+###
+
+def train(env_name):
+    num_episodes = 10000
+    max_timestep = 1000
+    rew_threshold = 0.9 * max_timestep
+    # create environment
     env = gym.make(env_name)
+    env._max_episode_steps = max_timestep
+    # algo
+    hyper_params = {'hidden_layers': [16,16]}
+    algo = REINFORCE(env, hyper_params=hyper_params)
+    algo.do_n_epochs(num_episodes=num_episodes, 
+                     max_timestep=max_timestep, 
+                     rew_threshold=rew_threshold
+                     )
+    return algo
+
+def valid(algo):
+    policy = algo.policy
+    policy.eval()
+    env = algo.env
     frames = []
-    pi.eval()
+    policy.eval()
     state = env.reset()
     while True:
-        action = pi.act(state)
+        action = policy.act(state)
         state, rew, done, _ = env.step(action)
         env.render()
         frames.append(env.render(mode="rgb_array"))
         if done:
             break
-    file_name = env_name+'_REINFORCE.gif'
-    save_frames_as_gif(frames, filename=file_name)
+    file_name = env.unwrapped.spec.id+'_REINFORCE.gif'
+    save_frames_as_gif(frames, path='images/', filename=file_name)
 
 ###
 
 if __name__ == '__main__':
-    EPISODE_NUM = 10000
-    MAX_TIMESTEP = 1000
-    REW_THRWSHOLD = 0.9 * MAX_TIMESTEP
-
     env_name = 'CartPole-v1' # solved
     # env_name = 'Acrobot-v1'  # solved
-    # env_name = 'MountainCar-v0'
-    hidden_layers = [32]
-    learning_rate = 0.003
-    discount_rate = 0.99
+    
+    algo = train(env_name)
 
-    env = gym.make(env_name)
-    env._max_episode_steps = MAX_TIMESTEP
-
-    pi = REINFORCE(env, hidden_layers, learning_rate, discount_rate)
-
-    valid(pi, env_name)
+    valid(algo)
